211767fce388e18cedb8cbcf4bc4a70e867d519c
Sam Perry <sperry53@ivytech.edu>
2024-02-12 20:29:44 -0500

    added html files

--- streams.js
+++ streams.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+`const fs = require('fs');`
 
 const readStream = fs.createReadStream('./docs/blog3.txt', { encoding: 'utf8'});
 const writeStream = fs.createWriteStream('./docs/blog4.txt');

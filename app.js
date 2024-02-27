7edd8c0ebe3cfa063581c78265735f1e4ffe1d64
Sam Perry <sperry53@ivytech.edu>
2024-02-26 21:27:20 -0500

    finished tutorial 10

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 const Blog = require('./models/blog');
+const { render } = require('ejs');
 
 // connect to mongodb
 const dbURI = 'mongodb+srv://sperry53:help@nodetuts.nsjxewe.mongodb.net/?retryWrites=true&w=majority&appName=nodetuts';
@@ -55,6 +56,7 @@ app.set('view engine', 'ejs');
 
 // middleware & static files
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true}));
 
 app.use((req, res, next) => {
   console.log('new request made:');
@@ -138,7 +140,40 @@ app.get('/blogs', (req, res) => {
     })
 })
 
+app.post('/blogs', (req, res) => {
+  const blog = new Blog(req.body);
 
+  blog.save()
+    .then((result) => {
+      res.redirect('/blogs');
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+})
+
+app.get('/blogs/:id', (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id)
+    .then(result => {
+      res.render('details', { blog: result, title: 'Blog Details' });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+})
+
+app.delete('/blogs/:id', (req, res) => {
+  const id = req.params.id;
+
+  Blog.findByIdAndDelete(id)
+    .then(result => {
+      res.json({ redirect: '/blogs' })
+    })
+    .catch(err => {
+      console.log(err);
+    })
+})
 
 
 app.get('/blogs/create', (req, res) => {
